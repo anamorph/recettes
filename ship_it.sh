2add7cb81342f9936449eb40c7fb0f8efb37b41f
anamorph <davinico@amazon.com.bh>
2023-07-18 14:29:22 +0300

    "Adding checksum to remove the need to systematically translate everything all over again."

--- ship_it.sh
+++ ship_it.sh
@@ -1,18 +1,42 @@
 #!/bin/sh
 # id:				ship.sh
 # author:			nicolas david - nicolas.david@anamor.ph
-# version: 			1
+# version history:
+# ----------------------------------------------------------------------------
+#					v1.0	nicolas@	Initial Version
+#					v1.1	nicolas@	Adding checksum to remove the need to
+#										systematically translate everything 
+#										all over again.
 #
-# purpose:			This script will launch the translation of cooking recipes
-#					from FR to both EN & AR. It will then ask for a commit me-
-#					ssage and push to the recipe repository.
+# purpose:
+# ----------------------------------------------------------------------------
+# This script will launch the translation of cooking recipes from FR to both 
+# EN & AR. It will then ask for a commit message and push to the recipe 
+# repository.
 # 
+# variables:
+# ----------------------------------------------------------------------------
+export dir=$(PWD)
+export recipe_checksum_file="$dir/ship_it-checksums"
+
+if [ ! -f "$recipe_checksum_file" ]; then
+		touch "$recipe_checksum_file"
+fi
+
 echo "### Translating recipes"
 for i in $(ls fr/*.md)
-    do
-        /opt/homebrew/bin/python3 translate_recipes.py $i en
-        /opt/homebrew/bin/python3 translate_recipes.py $i ar
-    done
+	do
+		export recipe_checksum=$(/usr/bin/shasum $i)
+		if grep -q $recipe_checksum $recipe_checksum_file; then
+			echo "-> [found!] original recipe hasn't changed. moving on."
+		else
+			echo "-> [not found!] original recipe is new or has changed."
+			/opt/homebrew/bin/python3 translate_recipes.py $i en
+			/opt/homebrew/bin/python3 translate_recipes.py $i ar
+			echo "adding checksum to file"
+			echo $recipe_checksum >> $recipe_checksum_file
+		fi
+	done
 
 echo "### Preparing recipes commit"
 git add .
